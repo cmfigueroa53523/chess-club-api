a452312e5552957f6d8ae3becba72eb5dec4da2f
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2024-07-28 14:00:17 +0300

    Change toSnakeCase function to throw error when input is not string

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,8 @@
 function toSnakeCase(str) {
-    return str.replace(/([A-Z])/g, '_$1').toLowerCase();
+  if(typeof str !== 'string') {
+    throw new Error(`str must be a string. Recieved: ${typeof str}`);
+  }
+  return str.replace(/([A-Z])/g, '_$1').toLowerCase();
 }
 
 export {
